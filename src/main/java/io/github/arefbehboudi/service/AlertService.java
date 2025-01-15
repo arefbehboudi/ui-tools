6ab2bcdd221d22132a9899028c32b07bfe39706f
arefbehboudi <behboodiaref@gmail.com>
2025-01-15 19:45:47 +0330

    fix: AutoImports file

--- src/main/java/io/github/arefbehboudi/service/AlertService.java
+++ src/main/java/io/github/arefbehboudi/service/AlertService.java
@@ -1,23 +1,13 @@
 package io.github.arefbehboudi.service;
 
 import io.github.arefbehboudi.model.Alert;
-import io.github.arefbehboudi.service.notification.MessageServiceFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
